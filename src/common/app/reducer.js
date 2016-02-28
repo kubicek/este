2d4a542e39401d2333787dab2c22b49d91ac35f8
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2016-02-28 02:08:01 +0100

    Adding redux-oauth + reducer

--- src/common/app/reducer.js
+++ src/common/app/reducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import { reduxFields } from '../lib/redux-fields';
 import { routerReducer as routing } from 'react-router-redux';
 
-import auth from '../auth/reducer';
+import {authStateReducer} from 'redux-auth';
 import device from '../device/reducer';
 import intl from '../intl/reducer';
 import todos from '../todos/reducer';
@@ -10,7 +10,7 @@ import ui from '../ui/reducer';
 import users from '../users/reducer';
 
 const appReducer = combineReducers({
-  auth,
+  auth: authStateReducer,
   device,
   intl,
   reduxFields,
